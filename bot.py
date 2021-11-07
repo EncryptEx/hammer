a70021b2d9e03380f3a70f6ac609eb7075da26dd
EncryptEx <limusina10@gmail.com>
2021-11-07 21:06:08 +0100

    fix error and improved code

--- bot.py
+++ bot.py
@@ -24,20 +24,18 @@ async def on_ready():
     print("HAMMER BOT Ready!", datetime.datetime.now())
     # sendNotifOwner("Bot UP:", OWNER)
     t = []
+    members_set = set()
     for s in bot.guilds:
         for c in s.channels:
-            if str(c.type) == "text":
+            if str(c.type) == 'text':
                 t.append(c)
-    members_set = set()
-    for guild in guilds:
-        for member in guild.members:
+        for member in s.members:
             members_set.add(member)
     members = len(members_set)
     print("I'm on:")
-    print(len(bot.guilds), "servers")
-    print(len(t), "channels")
-    print(members, "members")
-
+    print(len(bot.guilds),"servers")
+    print(len(t),"channels")
+    print(members,"members")
 
 debug = False
 
